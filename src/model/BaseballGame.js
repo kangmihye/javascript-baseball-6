ac81e765c0794acdfd0b6f0c43e9752acabb633c
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-29 18:03:45 +0900

    feat: 숫자를 비교하는 로직 구현
    - 사용자가 입력한 숫자와 랜덤 숫자를 비교해 볼인지 스트라이크인지 판단

--- src/model/BaseballGame.js
+++ src/model/BaseballGame.js
@@ -4,14 +4,26 @@ import baseballRandomNumberGenerator from "../service/baseballRandomNumberGenera
  * 게임을 관리하는 클래스
  */
 class BaseballGame {
-	#computerNum;
+	#computerNumbers;
 
 	constructor() {
-		this.#computerNum = baseballRandomNumberGenerator();
+		this.#computerNumbers = baseballRandomNumberGenerator(); //[1,2,3]
 	}
 
+	compareNumbers(userNumbers, result) {
+		this.#computerNumbers.forEach((num, idx) => {
+			if (num === userNumbers[idx]) return (result.ball += 1);
+			if (userNumbers.includes(num)) return (result.strike += 1);
+		});
+
+		return result;
+	}
 	/**
 	 * 사용자가 게임을 다시 시도할 때 사용하는 메서드
 	 */
 	retry() {}
 }
+
+export default BaseballGame;
+
+// console.log(new BaseballGame().compareNumbers([1, 2, 3], { strike: 0, ball: 0 }));
