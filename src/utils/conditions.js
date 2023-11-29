4ad416d5017a369b852a578ad365dec0d9a94435
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-29 16:56:19 +0900

    feat: 유효성 검사에 필요한 조건문 기능 구현

---
+++ src/utils/conditions.js
@@ -0,0 +1,14 @@
+import {} from "../utils/Constants.js";
+
+export const isInvalidNumber = (input) => {
+	// true면 숫자아님
+	input = Number(input.replaceAll(" ", ""));
+	return !Number.isInteger(input) || Number.isNaN(input);
+};
+
+export const isInvalidRange = (input) => {
+	//true면 범위 밖
+	input = Number(input.replaceAll(" ", ""));
+	return input < 1 || input > 31;
+};
+
