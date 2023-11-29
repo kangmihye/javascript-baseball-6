df056a486d2bcb5ecab6ee063e718748ed460faa
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-29 18:36:20 +0900

    feat: 게임 플레이 로직 구현

--- src/controller/ViewController.js
+++ src/controller/ViewController.js
@@ -1,15 +1,35 @@
 import InputView from "../view/InputView.js";
+import OutputView from "../view/OutputView.js";
 import inputErrorHandler from "../service/inputErrorHandler.js";
 import { validateNumbers } from "../service/validateInput.js";
+import InputViewService from "../service/InputViewService.js";
+import BaseballGame from "../model/BaseballGame.js";
 
 const ViewController = {
-	async gameStart() {
+	gameStart() {
 		InputView.printGameStart();
 	},
 
-	async playGame() {
+	async getUserNumber() {
 		const inputNumber = await inputErrorHandler(InputView.readNumber, validateNumbers);
-		console.log(inputNumber);
+		const parsedNumber = InputViewService.parseInput(inputNumber);
+		console.log("유저", parsedNumber);
+		return parsedNumber;
+	},
+
+	async playGame() {
+		const baseballGame = new BaseballGame();
+		let result = { strike: 0, ball: 0 };
+
+		while (1) {
+			result = { strike: 0, ball: 0 };
+			const userNum = await ViewController.getUserNumber();
+			result = baseballGame.compareNumbers(userNum, result);
+			console.log(result);
+			OutputView.printResultMessage(result);
+
+			if (result.strike === 3) break;
+		}
 	},
 };
 
