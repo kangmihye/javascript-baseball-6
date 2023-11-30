02e75fa0493e64791de86626816cc3540f634ea4
MiHye <57440192+kangmihye@users.noreply.github.com>
2023-11-30 16:05:52 +0900

    feat: 리플레이 게임을 입력받는 기능 구현

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,10 +1,11 @@
 export const INPUT_VIEW = Object.freeze({
-	startGame: "숫자 야구 게임을 시작합니다.\n",
 	askNumber: "숫자를 입력해주세요 : ",
+	askReplayingGame: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
 });
 
 export const OUTPUT_VIEW = Object.freeze({
-	printSomthing: "",
+	startGame: "숫자 야구 게임을 시작합니다.\n",
+	printGameOver: "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n",
 });
 
 export const ERROR = Object.freeze({
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,11 +2,11 @@ import { Console } from "@woowacourse/mission-utils";
 import { INPUT_VIEW } from "../utils/Constants.js";
 
 const InputView = {
-	printGameStart() {
-		Console.print(INPUT_VIEW.startGame);
-	},
 	async readNumber() {
 		return await Console.readLineAsync(INPUT_VIEW.askNumber);
+	},
+	async readReplayingGame() {
+		return await Console.readLineAsync(INPUT_VIEW.askReplayingGame);
 	},
 	// ...
 };
